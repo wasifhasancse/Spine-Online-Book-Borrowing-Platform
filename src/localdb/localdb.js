797e56eaed5144249944577fd9f2f7bc72b4165c
Wasif Hasan <wasifhasancse@gmail.com>
2026-05-01 18:11:44 +0600

    redeploy in vercel

--- src/localdb/localdb.js
+++ src/localdb/localdb.js
@@ -1,14 +1,14 @@
-export const galleryData = async () => {
-  const response = await fetch(
-    "spine-online-book-borrowing-platform-29kp0fdhp.vercel.app/bookData.json",
-  );
-  const booksData = await response.json();
-  return booksData;
-};
+// export const galleryData = async () => {
+//   const response = await fetch(
+//     "spine-online-book-borrowing-platform-29kp0fdhp.vercel.app/bookData.json",
+//   );
+//   const booksData = await response.json();
+//   return booksData;
+// };
 
-export const categoryData = async () => {
-  const categoriesResponse = await fetch(
-    "spine-online-book-borrowing-platform-29kp0fdhp.vercel.app/bookCategory.json",
-  );
-  return await categoriesResponse.json();
-};
+// export const categoryData = async () => {
+//   const categoriesResponse = await fetch(
+//     "spine-online-book-borrowing-platform-29kp0fdhp.vercel.app/bookCategory.json",
+//   );
+//   return await categoriesResponse.json();
+// };
