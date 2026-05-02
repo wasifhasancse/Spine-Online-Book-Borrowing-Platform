c5c17b116092089e6b138010c5a1411ca14b77a2
Wasif Hasan <wasifhasancse@gmail.com>
2026-05-02 11:46:25 +0600

    try to deploy vercel

--- src/localdb/localdb.js
+++ src/localdb/localdb.js
@@ -1,16 +1,14 @@
-import { promises as fs } from "node:fs";
-import path from "node:path";
-
-const readPublicJson = async (fileName) => {
-  const filePath = path.join(process.cwd(), "public", fileName);
-  const fileContent = await fs.readFile(filePath, "utf8");
-  return JSON.parse(fileContent);
-};
-
 export const galleryData = async () => {
-  return await readPublicJson("bookData.json");
+  const response = await fetch(
+    "https://spine-online-book-borrowing-pl-git-b513cd-wasif-hasans-projects.vercel.app/bookData.json",
+  );
+  const booksData = await response.json();
+  return booksData;
 };
 
 export const categoryData = async () => {
-  return await readPublicJson("bookCategory.json");
+  const categoriesResponse = await fetch(
+    "https://spine-online-book-borrowing-pl-git-b513cd-wasif-hasans-projects.vercel.app/bookCategory.json",
+  );
+  return await categoriesResponse.json();
 };
